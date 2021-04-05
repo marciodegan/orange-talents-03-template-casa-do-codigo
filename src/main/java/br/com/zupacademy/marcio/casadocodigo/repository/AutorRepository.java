8189315b4bb74a143e183b3403aebbd048b53b07
marciodegan <marciodegan@yahoo.com>
2021-04-05 11:14:46 -0300

    Alterado extends para JpaRepository

--- src/main/java/br/com/zupacademy/marcio/casadocodigo/repository/AutorRepository.java
+++ src/main/java/br/com/zupacademy/marcio/casadocodigo/repository/AutorRepository.java
@@ -2,13 +2,14 @@ package br.com.zupacademy.marcio.casadocodigo.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.zupacademy.marcio.casadocodigo.model.Autor;
 
 @Repository
-public interface AutorRepository extends CrudRepository<Autor, Long>{
+public interface AutorRepository extends JpaRepository<Autor, Long> {
 
 	Optional<Autor> findByEmail(String email);
 	
