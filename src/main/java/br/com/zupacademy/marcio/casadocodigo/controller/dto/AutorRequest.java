5f212fad4cd9d58e1729f3a04145474f671502f0
marciodegan <marciodegan@yahoo.com>
2021-04-05 11:15:35 -0300

    refatorado com novas anotações e @UniqueEmail

---
+++ src/main/java/br/com/zupacademy/marcio/casadocodigo/controller/dto/AutorRequest.java
@@ -0,0 +1,45 @@
+package br.com.zupacademy.marcio.casadocodigo.controller.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import br.com.zupacademy.marcio.casadocodigo.model.Autor;
+import br.com.zupacademy.marcio.casadocodigo.validation.UniqueEmail;
+
+import java.util.Locale;
+
+
+public class AutorRequest {
+	@NotBlank
+	private String nome;
+	@NotBlank @Email @UniqueEmail
+	private String email;
+	@NotBlank @Size(max=400)
+	private String descricao;
+
+	public AutorRequest(@NotBlank String nome, @NotBlank @Email String email, String descricao) {
+		this.nome = nome;
+		this.email = email.toLowerCase();
+		this.descricao = descricao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public Autor converter() {
+		return new Autor(this.nome, this.email, this.descricao);
+	}
+	
+	
+	
+}
