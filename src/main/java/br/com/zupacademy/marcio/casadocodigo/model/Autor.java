6596a537904aa9be4e105ebca77121ac4a76f744
marciodegan <marciodegan@yahoo.com>
2021-04-05 11:13:23 -0300

    Refatorado model autor com anotações corrigidas

--- src/main/java/br/com/zupacademy/marcio/casadocodigo/model/Autor.java
+++ src/main/java/br/com/zupacademy/marcio/casadocodigo/model/Autor.java
@@ -11,34 +11,56 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+
 @Entity
 public class Autor {
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@NotBlank @Column(nullable = false)
+
+	@Column(nullable = false)
 	private String nome;
-	@NotBlank @Email @Column(unique=true, nullable = false)
+	
+	@Column(unique=true, nullable = false)
 	private String email;
-	@NotBlank @Size(max = 400)
+
+	@Column(length=400)
 	private String descricao;
-	private LocalDateTime dataCriacao = LocalDateTime.now(); 
+	
+	private LocalDateTime dataCriacao = LocalDateTime.now();
 
 	@Deprecated
-	public Autor() {
+	public Autor(){
 	}
 
-	public Autor(@NotBlank String nome, @NotBlank @Email String email,
-			@NotBlank @Size(max = 400) String descricao) {
+	public Autor(String nome, String email, String descricao) {
 		this.nome = nome;
 		this.email = email;
 		this.descricao = descricao;
 	}
 
-	@Override
-	public String toString() {
-		return "Autor [id=" + id + ", nome=" + nome + ", email=" + email + ", descricao=" + descricao + ", dataCriacao="
-				+ dataCriacao + "]";
+	public Long getId() {
+		return id;
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+	
+	
 }
+
+
 
