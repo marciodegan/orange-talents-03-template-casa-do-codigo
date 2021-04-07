7da31af5268ef0de6607cbbb28177963f2039a61
marciodegan <marciodegan@yahoo.com>
2021-04-07 13:30:32 -0300

    added método converter

--- src/main/java/br/com/zupacademy/marcio/casadocodigo/controller/dto/LivroRequest.java
+++ src/main/java/br/com/zupacademy/marcio/casadocodigo/controller/dto/LivroRequest.java
@@ -5,7 +5,10 @@ import br.com.zupacademy.marcio.casadocodigo.model.Categoria;
 import br.com.zupacademy.marcio.casadocodigo.model.Livro;
 import br.com.zupacademy.marcio.casadocodigo.validation.MustExist;
 import br.com.zupacademy.marcio.casadocodigo.validation.UniqueValue;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 
+import javax.persistence.EntityManager;
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 
@@ -29,6 +32,7 @@ public class LivroRequest {
     private String isbn;
 
     @Future
+//    @JsonFormat(pattern = "dd-MM-yyyy", shape = Shape.STRING) // não está retornando ainda
     private LocalDate dataPublicacao;
 
     @MustExist(klass=Categoria.class)
@@ -37,7 +41,7 @@ public class LivroRequest {
     @MustExist(klass=Autor.class)
     private Long autorId;
 
-    public LivroRequest(@NotBlank String titulo, @NotBlank @Size(max = 500) String resumo, String sumario, @NotBlank @Min(20) double preco, int paginas, @NotBlank String isbn, @Future LocalDate dataPublicacao, Long categoriaId, Long autorId) {
+    public LivroRequest(@NotBlank String titulo, @NotBlank @Size(max = 500) String resumo, String sumario, @NotNull @Min(20) double preco, @NotNull @Min(100) int paginas, @NotBlank String isbn, @Future LocalDate dataPublicacao, Long categoriaId, Long autorId) {
         this.titulo = titulo;
         this.resumo = resumo;
         this.sumario = sumario;
@@ -48,6 +52,11 @@ public class LivroRequest {
         this.categoriaId = categoriaId;
         this.autorId = autorId;
     }
+
+    //    public void setDataPublicacao(LocalDate dataPublicacao) {
+    //    this.dataPublicacao = dataPublicacao;
+    //    }
+
 
     public String getResumo() {
         return resumo;
@@ -61,10 +70,12 @@ public class LivroRequest {
         return isbn;
     }
 
+//    public Livro converter(EntityManager manager) { // código comentado é outra forma utilizando EntityManager
     public Livro converter() {
         Categoria categoria = new Categoria(categoriaId);
         Autor autor = new Autor(autorId);
-        System.out.println(autor.getEmail());
+//        Categoria categoria = manager.find(Categoria.class, categoriaId);
+//        Autor autor = manager.find(Autor.class, autorId);
         return new Livro(this.titulo, this.resumo, this.sumario,
                 this.preco, this.paginas, this.isbn, this.dataPublicacao, categoria, autor);
         }
