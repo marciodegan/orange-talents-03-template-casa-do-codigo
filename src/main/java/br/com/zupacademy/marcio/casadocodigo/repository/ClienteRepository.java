81378551dcad18e03f9fd298d2f8da5935372275
marciodegan <marciodegan@yahoo.com>
2021-04-15 20:51:48 -0300

    delete repository e dto não utilizados

--- src/main/java/br/com/zupacademy/marcio/casadocodigo/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.zupacademy.marcio.casadocodigo.repository;
-
-import br.com.zupacademy.marcio.casadocodigo.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
-}
